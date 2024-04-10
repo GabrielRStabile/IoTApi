b6f055703dce4651ea33d764dd9719dba977a4dc
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-04-10 08:04:50 -0300

    primeiro commit

---
+++ src/main/java/br/edu/utfpr/iotapi/IotapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.iotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/iotapi/IotapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.iotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

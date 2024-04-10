dec6e94161820d37c428515cb3f0db8a2b1cdb2d
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-04-10 13:29:02 -0300

    validations in GatewayDTO

--- src/main/java/br/edu/utfpr/iotapi/dto/GatewayDTO.java
+++ src/main/java/br/edu/utfpr/iotapi/dto/GatewayDTO.java
@@ -2,12 +2,12 @@ package br.edu.utfpr.iotapi.dto;
 
 import br.edu.utfpr.iotapi.models.Pessoa;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record GatewayDTO(
-    @NotBlank @Size(min = 2) String nome,
-    @NotBlank String descricao,
-    String endereco,
-    Pessoa pessoa) {
-
+        @NotBlank @Size(min = 2) String nome,
+        @NotBlank String descricao,
+        @NotBlank String endereco,
+        @NotNull Pessoa pessoa) {
 }

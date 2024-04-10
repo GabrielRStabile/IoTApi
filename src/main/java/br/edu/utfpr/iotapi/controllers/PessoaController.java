0c3c5665bcbaa1523c6f7dfc8c6070865bb1561a
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-04-10 08:39:48 -0300

    add: PessoaRepository and PessoaController created

---
+++ src/main/java/br/edu/utfpr/iotapi/controllers/PessoaController.java
@@ -0,0 +1,10 @@
+package br.edu.utfpr.iotapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+}

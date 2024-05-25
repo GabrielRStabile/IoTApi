d017e37360137f42949cec549b5763f2f07fc5f9
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-05-25 19:15:52 -0300

    fix: spring-security blocking swagger ui

--- src/main/java/br/edu/utfpr/iotapi/core/SecurityConfiguration.java
+++ src/main/java/br/edu/utfpr/iotapi/core/SecurityConfiguration.java
@@ -29,6 +29,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers(HttpMethod.POST, "/auth").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/auth").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
                         )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)

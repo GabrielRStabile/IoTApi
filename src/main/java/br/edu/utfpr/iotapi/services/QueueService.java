2f563dc541059b831c31cf08925916a6e1ef9257
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-06-24 13:42:52 -0300

    feat: Add WebSocket configuration for real-time messaging

--- src/main/java/br/edu/utfpr/iotapi/services/QueueService.java
+++ src/main/java/br/edu/utfpr/iotapi/services/QueueService.java
@@ -1,8 +1,10 @@
 package br.edu.utfpr.iotapi.services;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +12,18 @@ public class QueueService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private SimpMessagingTemplate template;
+
     @Value("${spring.rabbitmq.queue}")
     public String QUEUE;
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend(QUEUE, message);
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void receiveMessage(String message) {
+        template.convertAndSend("/topic/message", message);
     }
 }
